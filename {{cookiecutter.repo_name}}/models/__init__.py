ba05874424f3dcbb2a8491d1680ec59548e2c0df
kracekumar <me@kracekumar.com>
2013-11-20 00:55:23 +0530

    Restructured

--- {{cookiecutter.repo_name}}/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from flask.ext.sqlalchemy import SQLAlchemy
-from {{cookiecutter.repo_name}} import app
-from coaster.sqlalchemy import BaseMixin, BaseNameMixin, BaseScopedNameMixin, BaseScopedIdNameMixin, BaseScopedIdMixin
-
-db = SQLAlchemy(app)
